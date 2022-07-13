87aee5a8ab1470ee9571011fe06af25a5f107520
Samandar <samandarkhodjiyev@gmail.com>
2022-07-13 14:37:00 +0900

    post signals

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Follow
+from .models import Post, Follow,Stream
 # Register your models here.
 
 admin.site.register(Follow)
 admin.site.register(Post)
+admin.site.register(Stream)
--- main/models.py
+++ main/models.py
@@ -1,8 +1,13 @@
 from distutils.command.upload import upload
 from django.db import models
 from users.models import User
+import uuid
+from django.db.models.signals import post_save, post_delete
 # Create your models here.
+
+
 class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to='posts',blank = True, null = True)
     posted = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User,on_delete = models.CASCADE)
@@ -19,4 +24,27 @@ class Follow(models.Model):
     def __str__(self):
         return str(f"{self.follower}  started following {self.following}")
 
+class Stream(models.Model):
+    following = models.ForeignKey(User,on_delete=models.CASCADE,related_name="following_stream")
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="followed_user")
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    date = models.DateTimeField()
 
+    def add_stream(sender,instance,created,**kwargs):
+        post = instance
+        user = post.owner
+        followers = Follow.objects.filter(following=user)
+        if created:
+            for follower in followers:
+                Stream.objects.create(post=post,following=user,date=post.posted,user=follower.follower)
+
+    def update_stream(sender,instance,created,**kwargs):
+        post = instance
+        user = post.owner
+        followers = Follow.objects.filter(following=user)
+        if not created:
+            for follower in followers:
+                Stream.objects.update(post=post,following=user,date=post.posted,user=follower.follower)
+
+post_save.connect(Stream.add_stream,sender=Post)
+post_save.connect(Stream.update_stream,sender=Post)
