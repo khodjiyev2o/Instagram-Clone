45ddb3a13139f675689f5cf80f0ac3a2f988c410
Samandar <samandarkhodjiyev@gmail.com>
2022-07-15 05:52:26 +0900

    signals change

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Follow,Stream
+from .models import Post, Follow,Stream,Likes
 # Register your models here.
 
 admin.site.register(Follow)
 admin.site.register(Post)
 admin.site.register(Stream)
+admin.site.register(Likes)
--- main/models.py
+++ main/models.py
@@ -1,9 +1,11 @@
+from asyncio.windows_events import NULL
 from distutils.command.upload import upload
 from django.db import models
 from users.models import User
 import uuid
 from django.db.models.signals import post_save, post_delete,pre_delete
 # Create your models here.
+
 
 
 class Post(models.Model):
@@ -15,8 +17,18 @@ class Post(models.Model):
     likes = models.IntegerField(default=0)
 
     def __str__(self):
-        return str(f"This is a post by {self.owner} having {self.likes} likes")
+        return str(f"This is a post by {self.owner} having {self.likes}     likes")
 
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_likes")
+
+    class Meta:
+        unique_together = (
+            ('user', 'post'),
+        )
+     
 
 class Follow(models.Model):
     follower = models.ForeignKey(User,on_delete=models.CASCADE,related_name="follower")
@@ -39,7 +51,20 @@ class Stream(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     date = models.DateTimeField()
 
-    def add_stream(sender,instance,created,**kwargs):
+
+    def __str__(self):
+        return str(f"{self.following} created new post , it is being shown to {self.user}")
+    
+    def add_stream_by_follow(sender,instance,created,**kwargs):
+        follow = instance
+        follower = follow.follower
+        following = follow.following
+        posts = Post.objects.filter(owner=following)
+        if created:
+            for post in posts:
+                Stream.objects.create(post=post,following=following,date=post.posted,user=follower)
+
+    def add_stream_by_post(sender,instance,created,**kwargs):
         post = instance
         user = post.owner
         followers = Follow.objects.filter(following=user)
@@ -59,8 +84,8 @@ class Stream(models.Model):
 
 
 
-post_save.connect(Stream.add_stream,sender=Post)
+post_save.connect(Stream.add_stream_by_post,sender=Post)
 post_save.connect(Stream.update_stream,sender=Post)
-
+post_save.connect(Stream.add_stream_by_follow,sender=Follow)
 
 post_save.connect(Follow.follow_yourself,sender=User)
