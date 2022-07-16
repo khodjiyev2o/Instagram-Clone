d26e9a5be9365aa317d615c970f9ca50c35ee58e
Samandar <samandarkhodjiyev@gmail.com>
2022-07-17 08:44:45 +0900

    user registration ,login,logout

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('login',views.login,name='login'),
+    path('login',views.thelogin,name='login'),
     path('register',views.register,name='register'),  
+    path('logout',views.logout_user, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import RegistrationForm
 # Create your views here.
-def login(request):
-    return render(request,'users/login.html',{})
+
+def thelogin(request):
+      if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.success(request, ('Please enter correct credentials...!'))
+            return redirect('login')
+      
+      return render(request,'users/login.html',{})
 
 
 def register(request):
-    return render(request,'users/register.html',{})
+    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(password=password, username=username)
+            login(request, user)
+            return redirect('main')
+
+    return render(request, 'users/register.html', {"forms": form})
+    
+
+
+def logout_user(request):
+    logout(request)
+    messages.warning(request, 'You have been logged out!')
+    return redirect('login')
