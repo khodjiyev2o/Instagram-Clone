b610a667342cb08a9581a895c6f7125b4b786c52
Samandar <samandarkhodjiyev@gmail.com>
2022-07-19 21:41:19 +0900

    new changes

--- instagram/settings.py
+++ instagram/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,3 +146,6 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+
+django_heroku.settings(locals())
